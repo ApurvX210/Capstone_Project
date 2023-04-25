4cc7bc5091981cf19a8481baf75c50413506410c
ApurvX210 <apurvsharma210@gmail.com>
2023-04-25 22:54:48 +0530

    Final Commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,23 +1,21 @@
-import { useEffect }from 'react'
+import { useEffect } from 'react'
 import { useAuthContext } from "../hooks/useAuthContext"
-import i from '../images/side.webp'
+import i from '../images/store3.png'
+// import i from '../images/back.jpg'
 
 const Home = () => {
-  const {user} = useAuthContext()
+  const { user } = useAuthContext()
   return (
     <div className="home">
       <div className='Hm'>
-        <h2>Buy Delicious,Fresh, Gourmet</h2>
-        <h2>Foods</h2>
+        <h2>Buy delicious and fresh food</h2>
+
         <p>Upto 40% Discount Offer Over All Latest Product</p>
-        <button>Shop Now</button>
+        <button class="btn">Shop Now</button>
       </div>
       <img className="circle-img" src={i} alt="avatar_img" />
     </div>
-    // <div class="container">
-    // <img src='back.jpg'></img>
-    // <div class="centered">Centered</div>
-  
+   
   )
 }
 
